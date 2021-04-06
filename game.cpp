ff0ebe2929c26a505457aef808e06324123d9fdb
Lui Ka On, Dexter <zaoxenn@hku.hk>
2021-04-06 19:39:28 +0800

    Add implementation of core procedures of the game
    Add the main game running function. It keeps prompting the user for a
    move command and update a grid accordingly, until it checked that the
    cells inside a grid is in full order again.

---
+++ game.cpp
@@ -0,0 +1,132 @@
+// ENGG1340/COMP2113 Programming Technologies - Group Project
+// Authors: Lui Ka On (3035074610), Ye Zhongzheng (3035782176)
+// Group Project: MessyGrid
+// Group: 165
+//
+// Description:
+// Implementing the game.
+
+#include <iostream>
+#include "game.h"
+#include "clear_screen.h"
+using namespace std;
+
+
+void RunGame()
+{
+  const int ROW = 5;
+  const int COL = 5;
+
+  // TODO: Init grid
+  int grid[5][5];
+
+  while ( !GridIsInOrder(grid, ROW, COL) ) {
+
+    ClearScreen();
+
+    bool valid_cmd = false;
+    char cmd = '\0';
+    while (!valid_cmd) {
+      // TODO: print grid
+
+      char cmd;
+      cmd = ReadMoveCommand();
+
+      switch (cmd) {
+        case 'W':
+        case 'S':
+        case 'A':
+        case 'D':
+          valid_cmd = true;
+          break;
+        default:
+          cout << endl << "Invalid input!" << endl << endl;
+      }
+    }
+
+    MoveCell(grid, ROW, COL, cmd);
+  }
+
+  cout << endl << "You reordered the messy grid. Congratulation!" << endl;
+}
+
+bool GridIsInOrder(int grid[][5], int row, int col)
+{
+  int correct_value = 1;
+  for (int i = 0; i < row; ++i)
+    for (int j = 0; j < col; ++j)
+      // if it is not the last cell and the cell has incorrect value:
+      if ((i != row || j != col) && grid[i][j] != correct_value++)
+        return false;
+  return true;
+}
+
+void MoveCell(int grid[][5], int row, int col, char cmd)
+{
+  Cell empty_cell;  // store the row and col index of the empty cell
+  for (int i = 0; i < row; ++i) {
+    for (int j = 0; j < col; ++j) {
+      if (!grid[i][j]) {
+        empty_cell.row = i;
+        empty_cell.col = j;
+        break;
+      }
+    }
+  }
+
+  Cell move_cell;  // store the row and col index of the cell to be moved
+  move_cell = empty_cell;
+
+  switch (cmd) {
+    case 'W':
+      move_cell.row += 1;
+      break;
+    case 'S':
+      move_cell.row -= 1;
+      break;
+    case 'A':
+      move_cell.col += 1;
+      break;
+    case 'D':
+      move_cell.col -= 1;
+      break;
+    default:
+      cout << "game.cpp: MoveCell(): invalid move command" << endl;
+      return;
+  }
+
+  if (IsValidCell(row, col, move_cell))
+    SwapCell(grid, empty_cell, move_cell);
+}
+
+char ReadMoveCommand()
+{
+  char input;
+
+  cout << "Please move the cell." << endl;
+  cout << "W: Up" << endl;
+  cout << "S: Down" << endl;
+  cout << "A: Left" << endl;
+  cout << "D: Right" << endl;
+  cin >> input;
+  // TODO: clear all remaining inputs in cin
+
+  return input;
+}
+
+void SwapCell(int grid[][5], Cell a, Cell b)
+{
+  // TODO: add guardian to prevent out of bound error when given invalid cell
+  int tmp = grid[a.row][a.col];
+  grid[a.row][a.col] = grid[b.row][b.col];
+  grid[b.row][b.col] = tmp;
+}
+
+bool IsValidCell(int row, int col, Cell cell)
+{
+  if (cell.row < 0 || cell.row >= row)
+    return false;
+  if (cell.col < 0 || cell.col >= col)
+    return false;
+  return true;
+}
---
+++ game.h
@@ -0,0 +1,59 @@
+// ENGG1340/COMP2113 Programming Technologies - Group Project
+// Authors: Lui Ka On (3035074610), Ye Zhongzheng (3035782176)
+// Group Project: MessyGrid
+// Group: 165
+//
+// Description:
+// Declare functions for running the game MessyGrid
+
+#ifndef GAME_H
+#define GAME_H
+
+// Storing the row and col index of a cell in the grid
+struct Cell {
+  int row;
+  int col;
+};
+
+// Function: Main function to run the game
+void RunGame();
+
+// Function: Accept user input and move the cell in the grid accordingly
+// Input: int grid[][5]: a 2D array representing the grid
+//        int row: the number of rows
+//        int col: the number of columns
+//        char cmd: the move command:
+//                  W: move cell below up
+//                  S: move cell above down
+//                  A: move cell on the right left
+//                  D: move cell on the left right
+void MoveCell(int grid[][5], int row, int col, char cmd);
+
+// Function: Check if a grid is in order
+// Input: int grid[][5]: a 2D array storing the board position
+//        int row: the number of rows
+//        int col: the number of columns
+// Output: bool: true if the grid is in order, false otherwise
+bool GridIsInOrder(int grid[][5], int row, int col);
+
+// Function: Prompt user to enter a character to move the cell
+// Output: Return a character representing a valid move command:
+//         W: move the cell below up
+//         S: move the cell above down
+//         A: move the cell on the right left
+//         D: move the cell on the left right
+char ReadMoveCommand();
+
+// Function: swap the value of two cells in a grid
+// Input: int grid[][5]: a 2D array storing the board position
+//        Cell x, Cell y: the two cells whose values to be swapped
+void SwapCell(int grid[][5], Cell a, Cell b);
+
+// Function: check if a given cell is valid (within the range of the grid)
+// Input: int row: the number of rows of the grid
+//        int col: the number of columns of the grid
+//        Cell cell: the cell to be checked
+// Output: true if the cell is within the dimension of the grid, false otherwise
+bool IsValidCell(int row, int col, Cell cell);
+
+#endif  // ifndef GAME_H
